4221f7794a88ffe21eb181d03a9b948cfc79fac6
Fenshu28 <fenshu28>
2025-11-21 02:26:50 -0600

    Corrige redirección segun el auth del usuario.

--- src/router/PublicRouter.js
+++ src/router/PublicRouter.js
@@ -1,10 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/auth/Login";
+import useAuth from "../hooks/useAuth";
 
 const PublicRouter = () => {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Cargando...</div>; // TODO: Implementar un spinner/skeletor
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route
+        path="/"
+        element={user ? <Navigate to="/App/inicio" /> : <Login />}
+      />
     </Routes>
   );
 };
