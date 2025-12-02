260f683f9187280a81223f4a79848b3a0a94606a
Fenshu28 <fenshu28>
2025-12-02 02:28:51 -0600

    Mejoramos diseño del datepicker.

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -5,7 +5,7 @@ const DatePicker = ({ id, value, onChange, disabled = false, label, required = f
   return (
     <div>
       {label && (
-        <label className="mb-2.5 block text-black dark:text-white">
+        <label htmlFor={id} className="mb-2.5 block text-black dark:text-white">
           {label} {required && <span className="text-meta-1">*</span>}
         </label>
       )}
@@ -16,11 +16,12 @@ const DatePicker = ({ id, value, onChange, disabled = false, label, required = f
           value={value}
           onChange={onChange}
           disabled={disabled}
+          required={required}
+          style={{
+            colorScheme: 'light dark'
+          }}
           className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary dark:disabled:bg-form-input"
         />
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <Icon icon="mdi:calendar" className="text-gray-400" width="20" />
-        </div>
       </div>
     </div>
   );
